5299060ce34e8997774fa752b6126003369f9360
Maria-Kolesnikova <149296584+Maria-Kolesnikova@users.noreply.github.com>
2023-11-12 22:46:40 +0300

    Лабораторная №3

---
+++ Лаб3/1.py
@@ -0,0 +1,14 @@
+# TODO Напишите функцию для поиска индекса товара
+def funct(list, pos):
+    for name in list:
+        if name == pos:
+            return list.index(name)
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = funct(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
